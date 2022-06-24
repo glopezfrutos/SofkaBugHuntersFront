0ab2166eb043101bf46e39878dc007a2d13e1c6f
David <depch47@gmail.com>
2022-06-24 13:11:00 -0500

    workin on login page

--- src/components/loginPage/LoginForm.tsx
+++ src/components/loginPage/LoginForm.tsx
@@ -1,12 +1,24 @@
 import * as React from "react"
 import {Button, Paper, PasswordInput, TextInput} from "@mantine/core";
+import {useForm} from "@mantine/form";
 
 interface IProps {
 }
 
 const LoginForm: React.FC<IProps> = () => {
+    const form = useForm({
+        initialValues: {
+            email: '',
+            password: '',
+        },
+        validate: {
+            email: (value) => (/^\S+@\S+$/.test(value) ? null : 'Invalid email'),
+        },
+    });
+
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault()
+        console.log(form.values)
     }
     return <>
         <Paper shadow="xs" p="xl">
@@ -14,11 +26,15 @@ const LoginForm: React.FC<IProps> = () => {
                 <TextInput
                     placeholder="Your email"
                     label="Email"
-                    required/>
+                    required
+                    {...form.getInputProps('email')}
+                />
+
                 <PasswordInput
                     placeholder="Password"
                     label="Password"
                     required
+                    {...form.getInputProps('password')}
                 />
                 <Button color="cyan" type="submit" mt="xs">
                     Login
