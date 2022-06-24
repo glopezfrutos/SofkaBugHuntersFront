8c61ede6dfb40fc5455fbf32a48927486c4ceacb
David <depch47@gmail.com>
2022-06-24 13:04:13 -0500

    workin on login page

--- src/components/loginPage/LoginForm.tsx
+++ src/components/loginPage/LoginForm.tsx
@@ -1,9 +1,34 @@
 import * as React from "react"
+import {Button, Paper, PasswordInput, TextInput} from "@mantine/core";
 
-interface IProps {}
+interface IProps {
+}
 
-const LoginForm : React.FC<IProps> = () => {
-    return<></>
+const LoginForm: React.FC<IProps> = () => {
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+    }
+    return <>
+        <Paper shadow="xs" p="xl">
+            <form onSubmit={(e) => handleSubmit(e)}>
+                <TextInput
+                    placeholder="Your email"
+                    label="Email"
+                    required/>
+                <PasswordInput
+                    placeholder="Password"
+                    label="Password"
+                    required
+                />
+                <Button color="cyan" type="submit" mt="xs">
+                    Login
+                </Button>
+            </form>
+            {/*<Center>*/}
+            {/*    <LoginWithGoogle/>*/}
+            {/*</Center>*/}
+        </Paper>
+    </>
 }
 
 export default LoginForm
--- src/components/loginPage/SignupForm.tsx
+++ src/components/loginPage/SignupForm.tsx
@@ -1,9 +1,35 @@
 import * as React from "react"
+import {Button, Paper, PasswordInput, TextInput} from "@mantine/core";
 
 interface IProps {}
 
 const SignupForm : React.FC<IProps> = () => {
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+    }
+
     return <>
+        <Paper shadow="xs" p="xl">
+            <form onSubmit={(e) => handleSubmit(e)}>
+                <TextInput
+                    placeholder="Your email"
+                    label="Email"
+                    required/>
+                <PasswordInput
+                    placeholder="Password"
+                    label="Password"
+                    required
+                />
+                <PasswordInput
+                    placeholder="Confirm password"
+                    label="Confirm Password"
+                    required
+                />
+                <Button color="cyan" type="submit" mt="xs">
+                    Sign up
+                </Button>
+            </form>
+        </Paper>
     </>
 }
 
